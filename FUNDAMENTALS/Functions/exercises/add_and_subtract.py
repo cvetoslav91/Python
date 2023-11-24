a62bedf0fe21b3a382a4fb23b49c9e23c0c7fc84
cvetoslav91 <cuko91@gmail.com>
2023-11-24 10:05:41 +0200

    functions

---
+++ FUNDAMENTALS/Functions/exercises/add_and_subtract.py
@@ -0,0 +1,14 @@
+def sum_numbers(a, b):
+    result = a + b
+    return result
+
+def subtract(result, c):
+    final_result = result - c
+    return final_result
+
+def add_and_subtract(first, second):
+    print(second)
+
+first = sum_numbers(a = int(input()), b = int(input()))
+second = subtract(first, c = int(input()))
+res = add_and_subtract(first, second)
