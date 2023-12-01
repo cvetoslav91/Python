fc844ac351287cfe7cc107b6efa3351583f627ce
cvetoslav91 <cuko91@gmail.com>
2023-12-01 18:25:37 +0200

    lists advanced

---
+++ FUNDAMENTALS/Lists Advanced/to_do_list.py
@@ -0,0 +1,12 @@
+my_list = []
+new_list = []
+final_list = []
+while True:
+    command = input()
+    if command == 'End':
+        break
+    else:
+        my_list.append(command)
+        my_list.sort(key=lambda x: int(x.split('-')[0]))
+result = [command.split('-')[1] for command in my_list]
+print(result)
