c865eb476181563fa0d9be8dcf9b66191b2f08e6
cvetoslav91 <cuko91@gmail.com>
2023-12-29 09:05:15 +0200

    text processing more exercises

---
+++ FUNDAMENTALS/Text Processing/More_Exercises/morse_code_translator.py
@@ -0,0 +1,35 @@
+message = input().split(' | ')
+morse_dictionary = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+
+for word in message:
+    letters_in_word = word.split()
+    for letter in letters_in_word:
+        print(morse_dictionary[letter], end="")
+    print(' ', end='')
