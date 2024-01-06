411bd460f92dedbdbbe64dab2a6a934ec5bb40e2
cvetoslav91 <cuko91@gmail.com>
2024-01-06 20:14:47 +0200

    regex exercises

--- FUNDAMENTALS/Regex/Exercises/variable_names.py
+++ FUNDAMENTALS/Regex/Exercises/variable_names.py
@@ -2,12 +2,8 @@ import re
 
 text = input()
 
-pattern = r'\b(_)([a-zA-Z0-9]+)\b'
+pattern = r'\b_([a-zA-Z0-9]+)\b'
 
-result = re.finditer(pattern, text)
-final_list = []
-for i in result:
-    to_add = i.group(2)
-    final_list.append(to_add)
+result = re.findall(pattern, text)
 
-print(','.join(final_list))
+print(','.join(result))
