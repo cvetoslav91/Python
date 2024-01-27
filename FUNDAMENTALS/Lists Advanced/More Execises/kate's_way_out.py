6dbe4e8bc8f524255c32a7199a9e8dc72beccb24
cvetoslav91 <cuko91@gmail.com>
2024-01-27 10:10:50 +0200

    multidimensional lists

--- FUNDAMENTALS/Lists Advanced/More Execises/kate's_way_out.py
+++ FUNDAMENTALS/Lists Advanced/More Execises/kate's_way_out.py
@@ -1,41 +1,71 @@
-import copy
-def move_up(kate_row, upper_row):
-    index = kate_row.index("k")
-    while upper_row[index] != ' ':
-        kate_row[index], upper_row[index] = upper_row[index], kate_row[index]
-        return upper_row, kate_row
+def can_move(cur_row, cur_col, lab):
+    if 0 <= cur_row < len(lab[0]) and 0 <= cur_col < len(lab[0]):
+        return True
+    return False
 
+def check_free_spaces(cur_row, cur_col, lab, free = []):
+    if cur_row - 1 >= 0:
+        if lab[cur_row - 1][cur_col] == ' ':
+            free.append('up')
+    if cur_row + 1 < len(lab):
+        if lab[cur_row + 1][cur_col] == ' ':
+            free.append('down')
+    if cur_col - 1 >= 0:
+        if lab[cur_row][cur_col - 1] == ' ':
+            free.append('left')
+    if cur_col + 1 < len(lab[0]):
+        if lab[cur_row][cur_col + 1] == ' ':
+            free.append('right')
+    return free
 
-def move_right(kate_row):
-    index = kate_row.index('k')
-    while kate_row[index + 1] != ' ':
-        kate_row[index], kate_row[index + 1] = kate_row[index + 1], kate_row[index]
-        return kate_row
-
-
-def move_down(kate_row, under_row):
-    index = kate_row.index("k")
-    while under_row[index] != " ":
-        kate_row[index], under_row[index] = under_row[index], kate_row[index]
-        return under_row, kate_row
-
-def move_left(kate_row):
-    index = kate_row.index('k')
-    moves = 0
-    while kate_row[index - 1] != ' ':
-        kate_row[index], kate_row[index - 1] = kate_row[index - 1], kate_row[index]
-        moves += 1
-        return kate_row
+def check_if_trapped(cur_row, cur_col, lab):
+    if cur_row - 1 >= 0 and cur_col - 1 >= 0 and cur_row + 1 < len(lab) and cur_col + 1 < len(lab[0]):
+        return True
+    return False
 
 
 rows = int(input())
-final_list = []
-for current_row in range(1, rows + 1):
-    new_row = input()
-    row_list = [x for x in new_row]
-    final_list.append(row_list)
-    if 'k' in new_row:
-        kate_row = current_row - 1
+labyrinth = []
+for row in range(rows):
+    current_row = list(input())
+    labyrinth.append(current_row)
+    if 'k' in current_row:
+        index = current_row.index('k')
+        kates_position = [row, index]
+current_row = kates_position[0]
+current_col = kates_position[1]
+moves = 0
+moves_dict = {
+    'up': [-1, 0],
+    'left': [0, -1],
+    'right': [0, 1],
+    'down': [1, 0]
+}
+while kates_position:
+    current_row = kates_position.pop(0)
+    current_col = kates_position.pop(0)
+    if can_move(current_row, current_col, labyrinth):
+        free_spaces = check_free_spaces(current_row, current_col, labyrinth)
+        if free_spaces:
+            for current_free_space in free_spaces:
+                labyrinth[current_row + moves_dict[current_free_space][0]][current_col + moves_dict[current_free_space][1]] = 'k'
+            labyrinth[current_row][current_col] = '#'
+            free_spaces.clear()
+            moves += 1
+            for r in range(len(labyrinth)):
+                for c in range(len(labyrinth[0])):
+                    if labyrinth[r][c] == 'k':
+                        kates_position.append(r)
+                        kates_position.append(c)
+        else:
+            if check_if_trapped(current_row, current_col, labyrinth):
+                if kates_position:
+                    continue
+                else:
+                    print('Kate cannot get out')
+            else:
+                print(f'Kate got out in {moves + 1} moves')
 
-empty_squares = [final_list[x][y] for x in final_list for y in x if x == ' ']
-print(empty_squares)
+    else:
+        if not kates_position:
+            print('Kate cannot get out')
