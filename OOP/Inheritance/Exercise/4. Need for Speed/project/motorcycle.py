122279a23e5b5ada9b879ce55d0f9abac2e8e954
cvetoslav91 <cuko91@gmail.com>
2024-02-29 15:51:22 +0200

    initial commit

---
+++ OOP/Inheritance/Exercise/4. Need for Speed/project/motorcycle.py
@@ -0,0 +1,8 @@
+from project.vehicle import Vehicle
+
+
+class Motorcycle(Vehicle):
+    pass
+
+
+
---
+++ OOP/Inheritance/Exercise/4. Need for Speed/project/test.py
@@ -0,0 +1,7 @@
+from project.family_car import FamilyCar
+from project.sport_car import SportCar
+from project.vehicle import Vehicle
+
+sport_car = SportCar(100, 15)
+sport_car.drive(50)
+print(sport_car.fuel)
